65ac788a1bd9b4124d07dcb5a9cee238f041bc13
Anna Polyakova <poteryannaya.anna@yandex.ru>
2025-04-28 18:41:40 +0300

    first

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,52 @@
 package handlers
+
+import (
+	"io"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/AeddGynvael3110/sprint6/internal/service"
+)
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "index.html")
+}
+
+func UploadHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	file, _, err := r.FormFile("myFile")
+	if err != nil {
+		http.Error(w, "Unable to get file from form", http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		http.Error(w, "Unable to read file", http.StatusInternalServerError)
+		return
+	}
+
+	result, err := service.DetectAndConvert(string(data))
+	if err != nil {
+		http.Error(w, "Error while converting data", http.StatusInternalServerError)
+		return
+	}
+
+	// Создание директории, если она не существует
+	os.MkdirAll("output", os.ModePerm)
+
+	fileName := "output/" + time.Now().UTC().Format("2006-01-02_15-04-05") + ".txt"
+	err = os.WriteFile(fileName, []byte(result), 0644)
+	if err != nil {
+		http.Error(w, "Unable to write to file: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte("Конвертация завершена! Результат сохранён в " + fileName))
+}
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,30 @@
 package server
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/AeddGynvael3110/sprint6/internal/handlers"
+)
+
+type Server struct {
+	http.Server
+}
+
+func NewServer(logger *log.Logger) *Server {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handlers.IndexHandler)
+	mux.HandleFunc("/upload", handlers.UploadHandler)
+
+	return &Server{
+		Server: http.Server{
+			Addr:         ":8080",
+			Handler:      mux,
+			ErrorLog:     logger,
+			ReadTimeout:  5 * time.Second,
+			WriteTimeout: 10 * time.Second,
+			IdleTimeout:  15 * time.Second,
+		},
+	}
+}
